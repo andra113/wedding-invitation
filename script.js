ffb60f01d947a8ba2456791e2e5d89e246291443
Andra <andradwikasa@gmail.com>
2023-07-19 19:54:23 +0700

    added navbar and the logo

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const buttonCover = document.getElementById("buttonCover");
 const homePage = document.getElementById("home");
 const coverPage = document.getElementById("cover");
 const header = document.querySelector('.header');
+const navbar = document.querySelector('.navbar')
 
 // javascript manipulation to show home page when button " buka undangan" clicked and hide cover page
 buttonCover.addEventListener("click", function () {
   homePage.style.display = "flex";
   coverPage.style.display = "none";
   header.classList.toggle('active');
+  navbar.classList.toggle('active')
 });
