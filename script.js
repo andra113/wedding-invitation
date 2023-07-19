05884e49a258da0a895fffdac4672d2f61f018eb
Andra <andradwikasa@gmail.com>
2023-07-19 07:33:16 +0700

    polished things

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // getting id for js manipulation
 const buttonCover = document.getElementById("buttonCover");
 const homePage = document.getElementById("home");
-const coverPage = document.getElementById("cover")
+const coverPage = document.getElementById("cover");
+const header = document.querySelector('.header');
 
-// javascript manipulation to show home page when button " buka undagan" clicked and hide cover page
+// javascript manipulation to show home page when button " buka undangan" clicked and hide cover page
 buttonCover.addEventListener("click", function () {
   homePage.style.display = "flex";
   coverPage.style.display = "none";
+  header.classList.toggle('active');
 });
